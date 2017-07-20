c3ce96e409477cc1ed1067f89ae56310b7c8f581
Doug L <douglubaway@gmail.com>
2017-07-20 09:47:07 -0700

    Added content to ejs files, began SQL writing data

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const cheerio = require('cheerio');
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Middleware
 const app = express();
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(express.static('public'));
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: [ /* secret keys */ ],
+//
+//   // Cookie Options
+//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
+// }));
 
 //View Engine
 app.set('view engine', 'ejs');
@@ -18,15 +26,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 //Routes
 app.get('/', function(request, response) {
-  response.render('index', {})
+  response.render('index', {});
 });
 
 app.get('/signup', function(request, response) {
-  response.render('', {})
+  response.render('signup', {});
 });
 
 app.get('/login', function(request, response) {
-  response.render('', {})
+  response.render('login', {});
 });
 
 //Port
